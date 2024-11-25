eb2ea4c2179d81d31a950693a1f195b0000bdd22
Bharat Chaudhary <bhrt.7856@gmail.com>
2024-11-25 21:24:28 +0545

    dropdown categories and modals

--- src/Common/Header.jsx
+++ src/Common/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { GoSearch } from "react-icons/go";
 import { FaRegUser } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
@@ -14,8 +14,16 @@ import { PiWechatLogoLight, PiHandCoinsDuotone } from "react-icons/pi";
 import { SlLogin } from "react-icons/sl";
 import Categories from '../components/Categories';
 import { FaChevronLeft, FaAngleRight } from "react-icons/fa6";
+import LoginModal from '../components/LoginModal';
+import DropDownMenu from '../components/DropDownMenu';
+import SignUpModal from '../components/SignUpModal';
+import ForgetPasswordModal from '../components/ForgetPasswordModal';
 
 function Header() {
+  const [dropMenu, setDropMenu] = useState(false)
+  const [showLogin, setShowLogin] = useState(false)
+  const [showSignUp, setShowSignUp] = useState(false)
+  const [forgetPassword, setForgetPassword] = useState(false)
   const [showflag, setShowflag] = useState(false)
   const [active, setActive] = useState("")
   const alphabets = ["ALL","#","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
@@ -23,6 +31,18 @@ function Header() {
   const itemsPerPage = 7;
   const totalItems = alphabets.length;
   const visibleAlphabets = alphabets.slice(currentIndex, currentIndex+itemsPerPage)
+
+  useEffect(() => {
+    if (dropMenu || showLogin || showSignUp || forgetPassword) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [dropMenu,showLogin, showSignUp]);
+
   function prevPage () {
     if (currentIndex > 0) {
       setCurrentIndex(currentIndex - itemsPerPage);
@@ -37,14 +57,45 @@ function Header() {
   const disablePrev = currentIndex <= 0;
   const disableNext = currentIndex >= totalItems - itemsPerPage;
 
+  function closeLogin(){
+    setShowLogin(false)
+  }
+
+  function closeSignUp(){
+    setShowSignUp(false)
+  }
+
+  function closeMenu(){
+    setDropMenu(false)
+  }
+
+  function goBack(){
+    setForgetPassword(false)
+  }
+
+  function closeforgetPassword(){
+    setForgetPassword(false)
+    setShowLogin(false)
+  }
+
   return (
-    <div className='max-w-full'>
+<div className='mx-auto'>
+<div className={`max-w-full ${dropMenu || showLogin || showSignUp || forgetPassword === true? "overflow-hidden":""} `} >
       {/* top-navbar */}
-      <div className='container h-[50px] md:h-[72px] grid grid-cols-5 md:grid-cols-4 lg:grid-cols-12 my-auto items-center justify-between'>
-        <div className='flex items-center col-span-2 gap-2 md:col-span-1 lg:col-span-2'>
-        <IoMenuSharp className='flex text-2xl lg:hidden'/>
-        <Link to="/" className='min-h-[15px] min-w-[72px] md:h-[21px] md:w-[102px]'>
-          <img src="../header-logo.svg" alt="jevee logo" />
+      <div className='container h-[50px] md:h-[72px] grid grid-cols-5 md:grid-cols-4 lg:grid-cols-12 my-auto justify-between'>
+        <div className='relative flex my-auto col-span-2 gap-2 md:col-span-1 lg:col-span-2'>
+          <div>
+          <div onClick={()=>{setDropMenu(true)}}>
+          <IoMenuSharp className='flex text-2xl lg:hidden' />
+          </div>
+          { dropMenu && <div className="fixed top-0 left-0 shadow-lg z-50">
+                <DropDownMenu closeMenu={closeMenu}/>
+              </div>}
+          </div>
+          
+        
+        <Link to="/" className='min-h-[15px] mt-1 min-w-[72px] md:h-[21px] md:w-[102px]'>
+          <img src="https://www.jeevee.com/_next/static/media/logo.b826b85b.svg" alt="jevee logo" />
         </Link>
         </div>
         <ul className='flex items-center justify-between col-span-3 md:col-span-3 lg:col-span-10'>
@@ -54,7 +105,7 @@ function Header() {
          <ul className='transform -translate-x-1/2 -translate-y-1/2 top-[150px] left-1/2 w-[420px] absolute hidden group-hover:flex flex-col justify-center pt-10 z-30 px-4 bg-white'>
          <div className='flex items-center border p-2 rounded-full'>
          <GoSearch className='text-black text-xl'/>
-         <input type="search" className='outline-none bg-transparent px-2' placeholder='Search brands'/>
+         <input type="search" className='outline-none bg-transparent px-2 w-full' placeholder='Search brands'/>
          </div>
          
          <div className='relative'>
@@ -92,7 +143,7 @@ function Header() {
           </ul>
 
          </li>
-         <li className='hidden md:flex items-center gap-2 bg-[#F8F8F8] rounded-full py-1 pl-2 pr-10'>
+         <li className='hidden md:flex justify-between items-center gap-2 bg-[#F8F8F8] rounded-full py-1 pl-2 pr-10'>
          <GoSearch className='text-blue-400 text-xl'/>
          <div>
          <input type="search" className='outline-none text-sm bg-transparent px-2' placeholder='Search for medicine...'/>
@@ -103,44 +154,44 @@ function Header() {
          <li className='relative group cursor-pointer flex justify-between items-center gap-[1px]'><FaRegUser className='text-2xl md:text-3xl'/>
          <div className='hidden xl:flex flex-col'><p className='text-[12px]'>HelloUser</p>
          <h2 className='flex items-center gap-2 text-sm font-semibold'>My Account </h2>
-         <ul className='absolute border-x border-b bg-[#F8F8F8] hidden group-hover:flex flex-col top-10 right-0 z-30 rounded-md'>
+         <ul className='absolute border-x border-b bg-[#F8F8F8] hidden group-hover:flex flex-col top-10 right-0 z-30 pb-2 rounded-md'>
 <li className='px-3 py-2 flex justify-center items-center border-b hover:bg-[#F4F5F7]'>
   <div className='flex items-center gap-4 bg-gradient-to-r from-[#51CDE7] via-[#7895FC] to-[#C167FC] px-3 py-2 text-white rounded-full w-[250px]'>
   <IoFlame className='text-xl'/> <h2>Jeevee Offers</h2>
   </div>
 </li>
-<li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]'><FaRegUser className='text-xl'/><h2>Join Jeevee</h2></li>
+<li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]' onClick={()=>setShowSignUp(true)}><FaRegUser className='text-xl'/><h2>Join Jeevee</h2></li>
 <li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]'><LiaPhoneVolumeSolid className='text-xl'/><h2>Contact Us</h2></li>
 <li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]'><HiOutlineInformationCircle className='text-xl'/><h2>About Us</h2></li>
 <li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]'><MdOutlineNoteAlt className='text-xl'/><h2>Terms & Conditions</h2></li>
 <li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]'><GiNotebook className='text-xl'/><h2>Policies</h2></li>
 <li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]'><PiWechatLogoLight className='text-xl'/><h2>Feedback</h2></li>
 <li className='flex py-2 px-6 gap-4 items-center border-b hover:bg-[#F4F5F7]'><PiHandCoinsDuotone className='text-xl'/><h2>Sell on Jeevee</h2></li>
-<li className='flex py-2 px-6 gap-4 items-center border-b'><SlLogin className='text-xl'/><h2>Login</h2></li>
+<li className='flex py-2 px-6 gap-4 items-center' onClick={()=>setShowLogin(true)}><SlLogin className='text-xl'/><h2>Login</h2></li>
          </ul>
 
          </div>
          <IoIosArrowDown className='mt-2 md:mt-5'/>
          </li>
 
-         <li className='cursor-pointer'><BsCart2 className='text-2xl'/></li>
-         <li className='cursor-pointer'><PiBellRingingLight className='text-2xl'/></li>
+         <li className='cursor-pointer' onClick={()=>setShowLogin(true)}><BsCart2 className='text-2xl'/></li>
+         <li className='cursor-pointer'><PiBellRingingLight className='text-2xl' onClick={()=>setShowLogin(true)}/></li>
          <li className='cursor-pointer flex items-center gap-2 relative' onClick={()=>setShowflag(!showflag)}>
          <div className='h-[15px] w-[24px]'>
-          <img src="../us-flag.png" alt="" className='w-full h-full'/>
+          <img src="https://www.jeevee.com/_next/static/media/us_flag.937a74e4.png" alt="" className='w-full h-full'/>
          </div>
          <IoIosArrowDown />
          {
           showflag && <div className='absolute bg-white right-0 top-9 flex flex-col items-center justify-center py-1 border z-30 rounded-sm'>
             <div className='cursor-pointer flex gap-3 px-5 py-2 hover:bg-[#F4F5F7]'>
             <div className='h-[20px] w-[30px] bg-cover'>
-            <img src="../us-flag.png" alt="" className='w-full h-full'/>
+            <img src="https://www.jeevee.com/_next/static/media/us_flag.937a74e4.png" alt="" className='w-full h-full'/>
             </div>
             <h2 className='text-sm'>English</h2>
             </div>
            <div className='cursor-pointer flex gap-3 px-5 py-2 w-full hover:bg-[#F4F5F7]'>
            <div className='h-[20px] w-[30px] bg-cover'>
-            <img src="../np-flag.png" alt="" className='w-full h-full'/>
+            <img src="https://www.jeevee.com/_next/static/media/np_flag.e79b7bb9.png" alt="" className='w-full h-full'/>
             </div>
             <h2 className='text-sm'>नेपाली</h2>
            </div>
@@ -156,11 +207,66 @@ function Header() {
 </div>
 
       {/* bottom navbar */}
-      
-      {/* <Categories /> */}
-      <hr />
 
+      {/* <Categories /> */}
+
+      <hr />
     </div>
+   
+
+{/* Full-page overlay when dropMenu is true */}
+{dropMenu && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={()=>{
+            closeMenu()
+          }}
+        />
+      )}
+
+{showLogin  && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={()=>{
+            closeLogin()
+          }}
+        />
+      )}
+
+{showSignUp  && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={()=>{
+            closeSignUp()
+          }}
+        />
+      )}
+
+{forgetPassword  && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 z-40"
+          onClick={()=>{
+            closeforgetPassword()
+          }}
+        />
+      )}
+
+{/* Login Modal */}
+<div className='absolute mr-4 translate-x-1/2 left-1/2 right-1/2 inset-0 z-40'>
+{
+  showLogin && <LoginModal closeLogin={closeLogin} setShowSignUp={setShowSignUp} setForgetPassword={setForgetPassword}/>
+}
+</div>
+
+<div className='absolute mr-4 translate-x-1/2 left-1/2 right-1/2 inset-0 z-40'>
+{
+  forgetPassword && <ForgetPasswordModal setShowLogin={setShowLogin} closeforgetPassword={closeforgetPassword} goBack={goBack}/>
+}
+</div>
+
+<div className='absolute translate-x-1/2 left-1/2 right-1/2 inset-0 z-40'>
+{
+  showSignUp && <SignUpModal closeSignUp={closeSignUp} setShowLogin={setShowLogin} />
+}
+</div>
+
+</div>
   )
 
 }
